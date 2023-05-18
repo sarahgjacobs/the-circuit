24fa9b26d76ce7dc04f464a8ad55e57ba3b3979a
Sarah Jacobs <sgjacobs14@gmail.com>
2023-05-18 12:01:08 -0400

    added logo

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+function Home() {
+  return (
+    <section>
+      <h1>The Circuit</h1>
+      <div class="circle__box">
+  <div class="circle__wrapper circle__wrapper--right">
+    <div class="circle__whole circle__right"></div>
+  </div>
+  <div class="circle__wrapper circle__wrapper--left">
+    <div class="circle__whole circle__left"></div>
+  </div>
+</div>
+    </section>
+  );
+}
+
+export default Home;
