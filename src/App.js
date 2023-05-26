8fdf910a58903ddfc7c67c9d610c128c5b3d541b
Sarah Jacobs <sgjacobs14@gmail.com>
2023-05-26 17:20:43 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/Navbar';
 import Home from './components/Home';
 import Characters from './components/Characters';
 
 function App() {
   return (
     <Router>
-    {/* <Navbar /> */}
+    <Navbar />
     <Routes>
       <Route path='/'element={<Home />}/>
       <Route path='/characters'element={<Characters />}/>
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item"><a href="#">The Circuit</a></li>
+        <li className="nav-item"><a href="#">Home</a></li>
+        <li className="nav-item"><a href="#">Characters</a></li>
+        <li className="nav-item"><a href="#">Preview</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
